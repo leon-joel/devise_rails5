0359d52219c54eed6a625cb3f3ad595434b7e589
L.Joel <leopard10000@gmail.com>
2016-12-12 22:52:48 +0900

    Devise: ログイン後のページを変更＆before_actionでログイン確認（非ログイン時はrootにredirect） ※参考: http://qiita.com/cigalecigales/items/f4274088f20832252374

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  # サインイン確認 ※この手のactionは、万が一追加し忘れていても自動的に適用される「オプトアウト方式」がbetterな気がする。つまり、except: にした方が良いということ。
+  before_action :sign_in_required, only: [:show]
+
   def index
   end
 
